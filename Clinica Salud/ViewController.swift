ecf553dc5f756926d356e8d2127a56c367a78962
josep-rivera <josep.rivera.m@tecsup.edu.pe>
2026-04-29 11:38:04 -0500

    Proyecto base

---
+++ Clinica Salud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clinica Salud
+//
+//  Created by Tecsup on 29/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
